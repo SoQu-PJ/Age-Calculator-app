004f63e251b986e8b1e9a0856f0e7905f5c48f0f
SoQu-PJ <Maniek999zano@gmail.com>
2023-06-03 14:01:54 +0200

    make style to DateResult

--- src/container/DateResult.tsx
+++ src/container/DateResult.tsx
@@ -1,9 +1,9 @@
 const DateResult = () => {
     return (
         <section className='date-result'>
-            <p className="year">-- years</p>
-            <p className="month">-- months</p>
-            <p className="day">-- days</p>
+            <p id="year" className="date"><span className="cal-result">38</span> years</p>
+            <p id="month" className="date"><span className="cal-result">3</span> months</p>
+            <p id="day" className="date"><span className="cal-result">26</span> days</p>
         </section>
     )
 }
