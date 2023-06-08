8fc12973e5b228302efee73382c7274152f8cb86
SoQu-PJ <Maniek999zano@gmail.com>
2023-06-08 22:40:37 +0200

    add simple animation to button and inputs, add rwd breakpoint: 1440px

--- src/container/DateSettings.tsx
+++ src/container/DateSettings.tsx
@@ -3,17 +3,17 @@ const DateSettings = () => {
         <form className="date-settings">
             <fieldset className="data-fieldset">
                 <label htmlFor="days">Day</label>
-                <input type="number" name="days" id="days" />
+                <input type="number" name="days" id="days" placeholder="DD" />
             </fieldset>
 
             <fieldset className="data-fieldset">
                 <label htmlFor="months">Month</label>
-                <input type="number" name="months" id="months" />
+                <input type="number" name="months" id="months" placeholder="MM" />
             </fieldset>
 
             <fieldset className="data-fieldset">
                 <label htmlFor="years">Year</label>
-                <input type="number" name="years" id="years" />
+                <input type="number" name="years" id="years" placeholder="YYYY" />
             </fieldset>
         </form>
     )
